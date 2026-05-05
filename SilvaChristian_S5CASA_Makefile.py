7770d0d645e30039c5f224a9dc49f764579eda23
Christian Silva <chsilvat@unal.edu.co>
2026-05-04 20:30:16 -0500

    corrige nombre datos1

--- SilvaChristian_S5CASA_Makefile.py
+++ SilvaChristian_S5CASA_Makefile.py
@@ -6,10 +6,10 @@ datos.dat:
 makedatos1:
 	g++ makedatos1.cpp -o makedatos1
 
-datos_cpp.dat: makedatos1
-	./makedatos1 > datos_cpp.dat
+datos1.dat: makedatos1
+	./makedatos1 > datos1.dat
 
-graficas: datos.dat datos_cpp.dat
+graficas: datos.dat datos1.dat
 	python plotdatos.py
 	python plotdatos1.py
 
@@ -17,4 +17,4 @@ resultados.pdf: graficas resultados.tex
 	pdflatex resultados.tex
 
 clean:
-	rm -f *.aux *.log *.out makedatos1 resultados.pdf datos.dat datos_cpp.dat
+	rm -f *.aux *.log *.out makedatos1 resultados.pdf datos.dat datos1.dat
